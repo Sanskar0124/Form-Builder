da5292261cfdfb2836448d78e2a41cc32ebdbe02
Sanskar-Sakhareliya <sanskar.sakhareliya@bjtmail.com>
2023-12-16 04:38:46 +0530

    Changes for envs

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const tasksRoutes = require('./routes/tasks');
 const formRoutes = require('./routes/forms');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,8 +13,23 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
+// Connect to MongoDBconst MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = process.env.MONGO_URI;
+if (!MONGO_URI) {
+    console.error('MONGO_URI environment variable is not defined.');
+    process.exit(1);
+}
+
+mongoose.connect(MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+})
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
 
 // Use tasks routes
 app.use('/tasks', tasksRoutes);
